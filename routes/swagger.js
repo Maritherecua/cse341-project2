e5e540ab8be6d179abbea5d0afc6a8e1cc15b4e7
Maritherecua <mrtrsarroyo@yahoo.com>
2026-09-23 21:02:44 -0700

    Dynamically set Swagger host and scheme based on request headers

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
+//Middleware to dynamically update host and protocol on every request
+router.use('/api-docs', (req, res, next) => {
+    swaggerDocument.host = req.get('host');
+    swaggerDocument.schemes = [req.protocol];
+    req.swaggerDocument = swaggerDocument;
+    next();
+}, swaggerUi.serve, swaggerUi.setup());
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+//router.use('/api-docs', swaggerUi.serve);
+//router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
