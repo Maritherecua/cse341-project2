6051530714d0e7fac57e14d44b14524ef1176093
Maritherecua <mrtrsarroyo@yahoo.com>
2026-09-23 21:34:13 -0700

    Adjusted host in swagger.js to use Render hostname and build-swagger script in package.json

--- routes/swagger.js
+++ routes/swagger.js
@@ -4,9 +4,11 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 //Middleware to dynamically update host and protocol on every request
 router.use('/api-docs', (req, res, next) => {
+    //Detect HTTPS when hosted behind Render's reverse proxy
+    const protocol = req.headers['x-forwarded-proto'] || req.protocol;
     //Dynamically set host and scheme on the document copy
     swaggerDocument.host = req.get('host');
-    swaggerDocument.schemes = [req.protocol];
+    swaggerDocument.schemes = [protocol];
     next();
 }, swaggerUi.serve, (req, res, next) => {
     //Pass the document to setup dynamically per request
